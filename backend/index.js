5d220cdec4afddb44800ad40e7b83c063c57d386
Abraham-Carreon <alejandrocarreon993@gmail.com>
2021-02-28 15:14:02 -0600

    Get de entrenadores terminado

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,43 @@
-import express from 'express';
+import express from 'express'
+import mysql from 'mysql'
 
 const app = express()
 
 const port = process.env.PORT || 5000
 
+const dbData = 
+{
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'inscripciones'
+}
+
 app.get('/entrenadores' , (req,res) =>
 {  
-    res.write("Datos")
-    res.end()
+    const connection = mysql.createConnection(dbData)
+
+    connection.connect((err) =>
+    {
+        if (err) console.log(err)
+
+        else 
+        {
+            console.log("Conexion hecha correctamente")
+
+            const selectTrainers = `select * from entrenadores`
+            connection.query(selectTrainers, (err, result, filelds) =>
+            {
+                if (err) console.log(err)
+
+                else
+                {
+                    console.log(result)
+                    res.json(result)
+                }
+            })
+        }
+    })
 })
 
 app.listen(port, () => console.log(`Servidor corriendo en http://localhost:${port}`))
