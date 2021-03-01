595f8552de13a60afd47b15339da59ca35b75b26
Abraham-Carreon <alejandrocarreon993@gmail.com>
2021-02-28 18:27:03 -0600

    Actualizar los datos de los deportistas

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const app = express()
 
 const port = process.env.PORT || 5000
 
+// Sirve para decodificar el json
+app.use(express.json())
 // Agrega body parser
 app.use(express.urlencoded({extended: true}))
 
@@ -129,6 +131,57 @@ app.get('/api/deportistas/:id' , (req,res) =>
                 {
                     // Retorna los deportes a el endpoint y tambien el status
                     res.status(200).json(result)                    
+                    
+                    connection.end((err) =>
+                    {
+                        if (err) console.log(err)
+                        
+                        else console.log("Conexion cerrada correctamente")
+                    })
+                }
+            })
+        }
+    })
+})
+
+app.put('/api/deportistas/:id' , (req,res) =>
+{  
+    const matricula = req.params.id
+
+    const { nombre, grupo, semestre, correo } = req.body
+    const connection = mysql.createConnection(dbData)
+
+    connection.connect((err) =>
+    {
+        if (err) 
+        {
+            console.log(err)
+            res.status(500).json( { Mensaje: "Error en la conexion de la base de datos"} )
+        }
+
+        else 
+        {
+            console.log("Conexion hecha correctamente")            
+            const updateAthetes = `
+                                    update deportistas 
+                                    set Matricula = ${matricula}, 
+                                    Nombre = '${nombre}', 
+                                    Grupo = ${grupo}, 
+                                    Semestre = ${semestre}, 
+                                    Correo = '${correo}' 
+                                    where Matricula = ${matricula} `
+
+            connection.query(updateAthetes, (err, result, fields) =>
+            {
+                if (err) 
+                {
+                    console.log(err)
+                    res.status(406).json( { Mensaje: "Hubo un error al actualizar o no existe el usuario"} )
+                }
+                else
+                {
+                    // Retorna los deportes a el endpoint y tambien el status
+                    res.status(200).json({ "Mensaje": `El usuario con la matricula ${matricula} ha sido actualizado`})                    
                     
                     connection.end((err) =>
                     {
