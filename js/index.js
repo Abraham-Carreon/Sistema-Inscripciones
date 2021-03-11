b989bee12f1622b0890c1c480ffaf35f3fd5ddc5
Abraham-Carreon <alejandrocarreon993@gmail.com>
2021-03-11 15:24:46 -0600

    Problemas con CORS de la api arreglado y validacion de datos con el login

--- js/index.js
+++ js/index.js
@@ -1,21 +1,46 @@
-document.addEventListener('DOMContentLoaded', () => 
-{
+document.addEventListener('DOMContentLoaded', () => {
     const boton = document.getElementById("enviar")
     boton.addEventListener('click', validar)
 })
 
-function validar()
-{
-    const matricula = document.querySelector('#matricula').value
-    const contrasena = document.querySelector('#contrasena').value
+async function validar() {
+    const matricula = document.querySelector('#matricula')
+    const contrasena = document.querySelector('#contrasena')
 
-    if (matricula !== "" && contrasena !== "")
-    {
-        console.log(matricula, contrasena)
-    }
+     if (matricula.value !== "" && contrasena.value !== "") {
+       
+            const url = "https://backend-inscripciones.herokuapp.com/api/administradores"
+            const datos = {
+                matricula: matricula.value,
+                contrasena: contrasena.value
+            }
 
-    else
-    {
+           const request = await fetch(url, {
+            method: 'POST', // or 'PUT'
+            body: JSON.stringify(datos), // data can be `string` or {object}!
+            headers:{
+            'Content-Type': 'application/json'
+            }
+        }).then(res => 
+          {
+              res.json()
+              if (res.ok) 
+              {
+                alert("Usuario validado")
+                window.location.href = "/src/principal.html"
+            } 
+                else {
+                alert("El usuario no existe, intente nuevamente")
+                matricula.value = ""
+                contrasena.value = ""
+            }
+          })
+          .then(response => console.log('Success:', response))
+          .catch(error => console.error('Error:', error))
+        
+    } else {
         alert("Formulario incompleto")
-    }
+    } 
 }
+
+
