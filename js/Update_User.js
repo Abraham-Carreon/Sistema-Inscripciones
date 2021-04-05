9132051408f00598ed4435354c34bfb1035b5e9f
HugoOvalle73 <hugo.73_@hotmail.com>
2021-04-05 18:02:22 -0500

    JS

---
+++ js/Update_User.js
@@ -0,0 +1,19 @@
+    const boton = document.getElementById("mybutton");
+
+    function validar()
+    {
+        var matricula = document.getElementById("matricula");
+
+        if (matricula == 1946342 || matricula == 1953829 || matricula == 1946218 || matricula == 1956753 || matricula == 1946887 || matricula == 1966811) 
+        {
+            document.body.style.background = "none"
+            alert("Matricula verificada")      
+        }
+        else 
+        {
+            document.body.style.background = "none"
+            alert("Matricula incorrecta, verifique de nuevo")
+        }
+    }
+
+    boton.addEventListener('click', validar)
