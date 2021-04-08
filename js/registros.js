66a1c3225f250bc0621aecf35f729cc5788a6ab4
ChrisDestroyer <64537705+ChrisDestroyer@users.noreply.github.com>
2021-04-08 17:37:30 -0500

    cambiosaaaaa

--- js/registros.js
+++ js/registros.js
@@ -5,17 +5,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
     async function validar()
 {
+    //hola
     const matricula=document.getElementById("matricula")
-    if (matricula == 1) 
-    /*if (matricula == 1) 
+    if(matricula.value == "")
     {
-        alert("Matricula incorrecta, verifique de nuevo")
+        if(deporte.value !== "---Seleccione una opcion---")
+        {
+            alert("Perfecto")
+        }
+        else
+        {
+            if(instructor.value !== "---Seleccione una opcion---")
+            {
+                alert("Perfecto")
+            }
+            else
+            {
+                alert("Falta un parametro")
+            }
+        }
     }
     else
     {
-        alert("Matricula correcta")
-        window.open("/src/Inscripcion_vemergente.html" , "ventana1" , "width=1000,height=600,scrollbars=NO")
-    */{
+        alert("Falta un parametro")
+    }
+    if (matricula == 1) 
+    {
         /*alert("Matricula correcta")
         window.open("/src/Inscripcion_vemergente.html" , "ventana1" , "width=1000,height=600,scrollbars=NO")*/
         const url = "https://backend-inscripciones.herokuapp.com/api/deportes"
