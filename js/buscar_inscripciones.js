45ed4dfc26cd0bfbc3a89018b0ca9b8d1d91db7f
ChrisDestroyer <64537705+ChrisDestroyer@users.noreply.github.com>
2021-04-07 19:55:08 -0500

    Eliminar inscripciones y modificacion en un id sobrante de update user

---
+++ js/buscar_inscripciones.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const boton = document.getElementById("buscar")
+    async function validar() {
+        let matricula = document.querySelector("#matricula")
+
+        if (matricula.value !== "") {
+            // document.body.style.background = "none"
+
+            const url = `https://backend-inscripciones.herokuapp.com/api/deportistas/${matricula.value}`
+            const request = await fetch(url)
+            const response = await request.json()
+            if (request.ok) {
+                alert("Usuario existe")
+                const nombre = document.querySelector('#nombre')
+                const grupo = document.querySelector('#grupo')
+                const semestre = document.querySelector('#semestre')
+                const correo = document.querySelector('#correo')
+
+                response.forEach(deportista => {
+                    nombre.value = deportista.Nombre
+                    grupo.value = deportista.Grupo
+                    semestre.value = deportista.Semestre
+                    correo.value = deportista.Correo
+                })
+            } else {
+                alert("Usuario no existe")
+            }
+        } else {
+            document.body.style.background = "none"
+            alert("Matricula incorrecta, verifique de nuevo")
+        }
+    }
+})
